7f1272c1aa9580bc9b26d372e48e3053dcf64b7d
orthros <colinnlsn@gmail.com>
2017-11-21 10:16:14 -0600

    Fixes and improvements to server.js Now Properly sending data to the service Now encoding the byte array properly scrape now takes a finder which is an object that has an async funciton, found. found takes an imageNum and img itself At this point, we're about ready to put everything into the message queue

--- dist-worker/server.js
+++ dist-worker/server.js
@@ -7,9 +7,9 @@ const queueName = process.env.QUEUE_NAME;
 
 async function main() {
   //TODO, get the bookNameChapter and baseURL from the message
-  const bookNameChapter = '/psyren/146';
-  const baseUrl = 'http://www.mangareader.net';
-  const onFound = function (img) { console.log('Found one!'); }
+  const bookName = 'psyren';
+  const chapter = 146;  
+  const baseUrl = 'http://www.mangareader.net';  
 
   // open.then(function (conn) {
   //   return conn.createChannel();
@@ -24,8 +24,24 @@ async function main() {
   //   });
   // }).catch(console.warn);
 
-  await getBookID('psyren');
-  await scrape(baseUrl, bookNameChapter, onFound);
+  var bookID = await getBookID(bookName);
+  var chapterID = await getChapterID(bookID,  chapter);
+
+  const finder = {
+    bookId: bookID,
+    chapterId: chapterID,    
+    found: async function(imgNum, img){
+      return await foundImage(this.bookId, this.chapterId, imgNum, img)
+    }
+  };
+
+  const finder2 = {
+    found: async function(imgNum, img){
+      console.log('Found an image ' + imgNum);
+    }
+  };
+
+  await scrape(baseUrl, '/' + bookName + '/' + chapter, finder);
 }
 
 async function getBookID(bookName) {
@@ -46,41 +62,64 @@ async function getBookID(bookName) {
   return res;
 }
 
-async function foundImage(bookID, imgNum, img) {
+async function getChapterID(bookID, chapterNumber){
   const endpoint = process.env.BOOKS_ENDPOINT;
   var options = {
     method: 'POST',
-    uri: endpoint + '/books/' + bookID + '/images',
+    uri: endpoint + '/books/' + bookID + '/chapters',
     headers: {
       'User-Agent': 'Request-Promise'
     },
     json: true,
     body: {
-      imageNumber: imgNum,
-      image: img
+      id: 0,
+      bookID : bookID,
+      chapterNumber: chapterNumber,
+      chapterTitle: ''
     }
-  }
-  var img = await rp(options).then(function (image) {
-    return image;
-  });
+  };
+  var res = await rp(options);
+  return res; 
 }
 
-async function scrape(baseUrl, bookNameChapter, onFound) {
+async function foundImage(bookID, chapterId, imgNum, img) {
+  const endpoint = process.env.BOOKS_ENDPOINT;
+  var options = {
+    method: 'POST',
+    uri: endpoint + '/books/' + bookID + '/chapter/' + chapterId + '/pages',
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true,
+    body: {
+      id:0,
+      chapterId: chapterId,
+      pageNumber: imgNum,
+      data: Array.prototype.slice.call(Buffer.from(img),0)
+    }
+  }
+  var response = await rp(options);
+}
+
+async function scrape(baseUrl, bookNameChapter, finder) {
   const imgSelecter = '#img';
   const totalSelecter = '#selectpage';
   const nextSelecter = '#imgholder > a:nth-child(1)';
   const totalRegex = /of (\d+)/g;
-
+  
+  //When we start scraping there is ALWAYS at least one page
+  var totalPages = 1;
   var nextLocation = baseUrl + bookNameChapter;
-  var html = await rp(nextLocation);
-  //Determine total number of pages
-  var $ = cheerio.load(html);
-  var totalPages = +totalRegex.exec($(totalSelecter).text())[1];
-
+  
   for (i = 0; i < totalPages; i++) {
+    //Navigate and load
     var html = await rp(nextLocation);
-    //Determine total number of pages
     var $ = cheerio.load(html);
+    
+    //Determine total number of pages
+    totalRegex.lastIndex = 0;
+    var totalPages = +totalRegex.exec($(totalSelecter).text())[1];
+    
     var imagePath = $(imgSelecter).attr('src');
 
     //We have the image's path 
@@ -96,7 +135,7 @@ async function scrape(baseUrl, bookNameChapter, onFound) {
     });
 
     //Now that we found it, let's go
-    onFound(img);
+    await finder.found(i, img);
 
     //Now we need to grab the next one
     nextLocation = baseUrl + $(nextSelecter).attr('href');
