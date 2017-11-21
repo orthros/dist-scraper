919a7f3191bfc78b8e78bb3c512f11998880339c
Colin <colinnlsn@gmail.com>
2017-11-21 06:48:58 -0600

    Added a new service books-service to store the books/pages Highly under dev Uses Marten and Postgresql .NET Core WebAPI app

---
+++ books-service/books-service/Controllers/BooksController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using books_service.Models;
+using Marten;
+
+namespace books_service.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Books")]
+    public class BooksController : Controller
+    {
+        public IDocumentStore Store { get; }
+
+        public BooksController(IDocumentStore store)
+        {
+            Store = store;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}", Name = "Get")]
+        public Book Get(int id)
+        {
+            return new Book()
+            {
+                Id = id,
+                Title = "value"
+            };
+        }
+
+        // POST: api/Books
+        [HttpPost]
+        public int Post([FromBody]Book value)
+        {
+            using (var session = Store.OpenSession())
+            {
+                var existing = session
+                    .Query<Book>()
+                    .Where(x => x.Title == value.Title)
+                    .FirstOrDefault();
+                if(existing == null)
+                {
+                    existing = new Book() { Title = value.Title };
+                    session.Store(existing);
+                    session.SaveChanges();
+                }
+
+                return existing.Id;
+            }
+        }
+        
+        // PUT: api/Books/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ books-service/books-service/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Marten;
+
+namespace books_service
+{
+    public class Startup
+    {
+        private const string MARTEN_CONNECTION_KEY = "BOOKSERV_SQLKEY";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IDocumentStore>(DocumentStore.For(_ =>
+            {
+                //_.Connection(Configuration.GetConnectionString(MARTEN_CONNECTION_KEY));
+                _.Connection("host=localhost;port=32774;database=marten_test;username=postgres");
+                _.CreateDatabasesForTenants(c =>
+                {
+                    //c.MaintenanceDatabase("");
+                    c.ForTenant()
+                        .CheckAgainstPgDatabase()
+                        .WithOwner("postgres")
+                        .WithEncoding("UTF-8")
+                        .ConnectionLimit(-1)
+                        .OnDatabaseCreated(__ =>
+                        {
+                            //dbCreated = true;  
+                        });
+                });  
+            }));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
